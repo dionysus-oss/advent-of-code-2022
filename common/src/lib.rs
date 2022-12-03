cde7e0e32cdde6682116394d878cc634877ddd9b
dionysus-oss <dionysus-oss@proton.me>
2022-12-03 02:13:50 +0000

    Format common

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -4,7 +4,9 @@ use std::io::BufRead;
 use std::path::Path;
 
 pub fn read_lines<P>(path: P) -> io::Result<io::Lines<io::BufReader<File>>>
-    where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(path)?;
     Ok(io::BufReader::new(file).lines())
 }
