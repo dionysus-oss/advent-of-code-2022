91c6fa6517260f42b34ea8ecf2aab7a8cf6c7c92
Dionysus OSS <dionysus-oss@proton.me>
2022-12-06 21:41:22 +0000

    Add day 6 solution

---
+++ day-6/src/main.rs
@@ -0,0 +1,39 @@
+#![feature(iter_next_chunk)]
+
+use common::read_lines;
+use std::collections::{HashSet, VecDeque};
+
+fn main() {
+    let input = read_lines("input.txt").unwrap();
+    let input_txt = input.last().unwrap().unwrap();
+
+    println!(
+        "number of chars 1 {}",
+        find_marker_pos::<4>(input_txt.as_ref())
+    );
+    println!(
+        "number of chars 2 {}",
+        find_marker_pos::<14>(input_txt.as_ref())
+    );
+}
+
+fn find_marker_pos<const N: usize>(input: &str) -> usize {
+    let mut stream = input.chars();
+
+    let mut buf = VecDeque::with_capacity(4);
+    buf.extend(stream.next_chunk::<N>().unwrap().iter());
+
+    let mut final_pos = N;
+    for (pos, ch) in stream.enumerate() {
+        let h: HashSet<char> = HashSet::from_iter(buf.iter().cloned());
+        if h.len() == N {
+            final_pos += pos;
+            break;
+        }
+
+        buf.pop_front();
+        buf.push_back(ch);
+    }
+
+    final_pos
+}
