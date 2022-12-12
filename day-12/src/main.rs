e4c309a220a51e5ee09a845ca412b2b11cd8ef40
Dionysus OSS <dionysus-oss@proton.me>
2022-12-12 23:56:46 +0000

    Add part 2 solution for day 12

--- day-12/src/main.rs
+++ day-12/src/main.rs
@@ -1,7 +1,6 @@
 use common::read_lines;
 use std::collections::{HashMap, HashSet};
 use std::env::args;
-use std::path::Path;
 
 type Node = (usize, usize);
 
@@ -16,7 +15,6 @@ fn main() {
         .enumerate()
         .flat_map(|(row_number, line)| {
             let line = line.unwrap();
-            println!("{}", line);
             let chars = line.chars();
             chars
                 .enumerate()
@@ -40,7 +38,21 @@ fn main() {
     println!("end at {:?}", end);
     println!();
 
-    println!("path length = {}", a_star_search(&grid, start, end, h));
+    println!(
+        "path length = {}",
+        a_star_search(&grid, start, end, h).unwrap()
+    );
+
+    let min_value: usize = grid
+        .clone()
+        .into_iter()
+        .filter(|(_, h)| *h == 0)
+        .map(|(start, _)| a_star_search(&grid, start, end, h))
+        .filter(|r| *r != None)
+        .map(|r| r.unwrap())
+        .min()
+        .unwrap();
+    println!("min value = {}", min_value);
 }
 
 fn a_star_search(
@@ -48,13 +60,10 @@ fn a_star_search(
     start: Node,
     end: Node,
     h: fn(Node, Node) -> usize,
-) -> usize {
+) -> Option<usize> {
     // Nodes still to consider
     let mut open_set: HashSet<Node> = HashSet::new();
     open_set.insert(start);
-
-    // Best paths from one node to another
-    // let mut trace: HashMap<Node, Node> = HashMap::new();
 
     let mut past_cost: HashMap<Node, usize> = HashMap::new();
     for n in grid.keys() {
@@ -75,12 +84,12 @@ fn a_star_search(
             }
         }
 
-        println!("considering {:?}", current);
-        println!("from open set {:?}", open_set);
+        // println!("considering {:?}", current);
+        // println!("from open set {:?}", open_set);
 
         if current == end {
-            println!("finished at {:?}", end);
-            return *past_cost.get(&end).unwrap();
+            // println!("finished at {:?}", end);
+            return Some(*past_cost.get(&end).unwrap());
         }
 
         open_set.remove(&current);
@@ -91,79 +100,78 @@ fn a_star_search(
         if current.1 > 0 {
             let left = (current.0, current.1 - 1);
             if valid_move(grid, current_height, &left) {
-                println!(
-                    "left is - {:?} with height {}",
-                    left,
-                    (grid.get(&left).unwrap() + b'a') as char
-                );
+                // println!(
+                //     "left is - {:?} with height {}",
+                //     left,
+                //     (grid.get(&left).unwrap() + b'a') as char
+                // );
                 neighbors.push(left);
             }
         }
 
         let right = (current.0, current.1 + 1);
         if valid_move(grid, current_height, &right) {
-            println!(
-                "right is - {:?} with height {}",
-                right,
-                (grid.get(&right).unwrap() + b'a') as char
-            );
+            // println!(
+            //     "right is - {:?} with height {}",
+            //     right,
+            //     (grid.get(&right).unwrap() + b'a') as char
+            // );
             neighbors.push(right);
         }
 
         if current.0 > 0 {
             let up = (current.0 - 1, current.1);
             if valid_move(grid, current_height, &up) {
-                println!(
-                    "up is - {:?} with height {}",
-                    up,
-                    (grid.get(&up).unwrap() + b'a') as char
-                );
+                // println!(
+                //     "up is - {:?} with height {}",
+                //     up,
+                //     (grid.get(&up).unwrap() + b'a') as char
+                // );
                 neighbors.push(up);
             }
         }
 
         let down = (current.0 + 1, current.1);
         if valid_move(grid, current_height, &down) {
-            println!(
-                "down is - {:?} with height {}",
-                down,
-                (grid.get(&down).unwrap() + b'a') as char
-            );
+            // println!(
+            //     "down is - {:?} with height {}",
+            //     down,
+            //     (grid.get(&down).unwrap() + b'a') as char
+            // );
             neighbors.push(down);
         }
 
-        for pn in &neighbors {
-            println!(
-                "possible neighbor - {:?} with height {}",
-                pn,
-                (grid.get(pn).unwrap() + b'a') as char
-            );
-        }
+        // for pn in &neighbors {
+        //     println!(
+        //         "possible neighbor - {:?} with height {}",
+        //         pn,
+        //         (grid.get(pn).unwrap() + b'a') as char
+        //     );
+        // }
 
         for neighbor in neighbors {
             let maybe_new_past_cost = past_cost.get(&current).unwrap() + 1;
 
-            println!(
-                "maybe new {}, current {}",
-                maybe_new_past_cost,
-                *past_cost.get(&neighbor).unwrap()
-            );
+            // println!(
+            //     "maybe new {}, current {}",
+            //     maybe_new_past_cost,
+            //     *past_cost.get(&neighbor).unwrap()
+            // );
             if maybe_new_past_cost < *past_cost.get(&neighbor).unwrap() {
-                //trace.insert(neighbor, current);
                 past_cost.insert(neighbor, maybe_new_past_cost);
                 future_cost.insert(neighbor, maybe_new_past_cost + h(neighbor, end));
 
                 if open_set.get(&neighbor) == None {
-                    println!("push new neighbor {:?}", neighbor);
+                    // println!("push new neighbor {:?}", neighbor);
                     open_set.insert(neighbor);
                 }
             }
         }
 
-        println!();
+        // println!();
     }
 
-    panic!("No path found!");
+    None
 }
 
 fn valid_move(grid: &HashMap<(usize, usize), u8>, current_height: &u8, move_to: &Node) -> bool {
